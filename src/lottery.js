b564175047b6df3bb18d92656cb51f7e22bce9c6
Max Proano <proanom89@gmail.com>
2021-04-20 11:08:29 -0700

    Connect to deployed contract

---
+++ src/lottery.js
@@ -0,0 +1,66 @@
+import web3 from './web3.js';
+
+const address = '0x2a0B69F55f986dec0b3B3955cD04F745208078e5';
+
+const abi = [
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "manager",
+    "outputs": [{
+        "name": "",
+        "type": "address"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  }, {
+    "constant": false,
+    "inputs": [],
+    "name": "pickWinner",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }, {
+    "constant": true,
+    "inputs": [],
+    "name": "getPlayers",
+    "outputs": [{
+        "name": "",
+        "type": "address[]"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  }, {
+    "constant": false,
+    "inputs": [],
+    "name": "enter",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+  }, {
+    "constant": true,
+    "inputs": [{
+        "name": "",
+        "type": "uint256"
+    }],
+    "name": "players",
+    "outputs": [{
+        "name": "",
+        "type": "address"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  }, {
+    "inputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  }
+];
+
+export default new web3.eth.Contract(abi, address)
